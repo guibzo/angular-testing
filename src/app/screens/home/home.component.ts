f5f7b66adc83fe953b44c68d0c5548af805e96eb
Guilherme Viana <imluizguilara@hotmail.com>
2025-01-25 18:41:41 -0300

    add cn etc

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,48 +1,50 @@
-import { ContentContainerComponent } from '@/components/content-container/content-container.component';
-import { HeaderComponent } from '@/components/header/header.component';
-import { CalculatorService } from '@/services/calculator/service';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, computed, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ContentContainerComponent } from '@/components/content-container/content-container.component'
+import { HeaderComponent } from '@/components/header/header.component'
+import { CalculatorService } from '@/services/calculator/service'
+import { cn } from '@/utils/cn'
+import { CommonModule, NgOptimizedImage } from '@angular/common'
+import { Component, computed, inject, signal } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
-  selector: 'app-home',
-  imports: [
-    HeaderComponent,
-    CommonModule,
-    NgOptimizedImage,
-    RouterLink,
-    ContentContainerComponent,
-  ],
-  templateUrl: './home.component.html',
+	selector: 'app-home',
+	imports: [HeaderComponent, CommonModule, NgOptimizedImage, RouterLink, ContentContainerComponent],
+	templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  public title = 'angular-testing';
-  public firstName = signal('bozo');
-  public firstNameCapitalized = computed(() => this.firstName().toUpperCase());
+	firstName = signal('bozo')
+	firstNameCapitalized = computed(() => this.firstName().toUpperCase())
+	isOutlineVariant = signal(false)
 
-  public age = 50;
-  public avatarURL = 'https://github.com/guibzo.png';
+	cn = cn
+	title = 'angular'
 
-  public headerCount = signal(0);
+	age = 50
+	avatarURL = 'https://github.com/guibzo.png'
 
-  public items = [
-    { id: 1, name: 'one' },
-    { id: 2, name: 'two' },
-    { id: 3, name: 'three' },
-  ];
+	headerCount = signal(0)
 
-  private calculatorService = inject(CalculatorService);
+	items = [
+		{ id: 1, name: 'one' },
+		{ id: 2, name: 'two' },
+		{ id: 3, name: 'three' },
+	]
 
-  public totalCost = this.calculatorService.sum(30, 55);
+	private calculatorService = inject(CalculatorService)
 
-  public printEvent(event: number) {
-    this.headerCount.set(event);
-    console.log(`printEvent ${event}`);
-  }
+	totalCost = this.calculatorService.sum(30, 55)
 
-  public sendForm(event: Event) {
-    event.preventDefault();
-    console.log(event);
-  }
+	handleChangeButtonVariant() {
+		this.isOutlineVariant.set(!this.isOutlineVariant())
+	}
+
+	printEvent(event: number) {
+		this.headerCount.set(event)
+		console.log(`printEvent ${event}`)
+	}
+
+	sendForm(event: Event) {
+		event.preventDefault()
+		console.log(event)
+	}
 }
